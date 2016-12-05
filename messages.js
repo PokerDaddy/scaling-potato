067100caaf5419fe98986dc067e4e8f84fb4a2f6
Septeract <ellie@shivergaming.com>
2016-12-05 15:22:19 -0800

    Add message cleaning function

--- messages.js
+++ messages.js
@@ -5,8 +5,13 @@ let messages = db.addCollection("messages");
 
 let _exports = module.exports = {};
 
+function clean_message(msg) {
+	delete msg.secret;
+	msg.timestamp = (new Date()).getTime() / 1000;
+}
+
 _exports.store_message = function (msg) {
-	messages.insert(msg);
+	messages.insert( clean_message(msg) );
 }
 
 _exports.get_messages = function (timestamp) {
