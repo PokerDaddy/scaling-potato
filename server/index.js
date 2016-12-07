9a4e72dc55d06d43bc4fa16d7a0325e665a1b43a
Septeract <ellie@shivergaming.com>
2016-12-07 14:29:09 -0800

    server/index.js: fix the... affront made to my code

--- server/index.js
+++ server/index.js
@@ -6,38 +6,38 @@ let app = express();
 app.use(body_parser.json())
 
 app.get('/update/:time', (req, res) => {
-  let time = req.params.time;
+	let time = req.params.time;
 
-  res.send(db.get_messages(time));
+	res.send(db.get_messages(time));
 });
 
 app.post('/send', (req, res) => {
-  let msg = req.body;
+	let msg = req.body;
 
-  console.log(msg);
+	console.log(msg);
 
-  if (db.store_message(msg) === false) {
-    res.status(400)
-    res.send("Invalid token")
-  } else {
-    res.send();
-  }
+	if (db.store_message(msg) === false) {
+	  res.status(400)
+	  res.send("Invalid token")
+	} else {
+	  res.send();
+	}
 });
 
 app.post('/login', (req, res) => {
-  let session = req.body;
-  db.generate_session(session);
+	let session = req.body;
+	db.generate_session(session);
 
-  res.send(JSON.stringify(session));
+	res.send(JSON.stringify(session));
 });
 
 app.get('/login', (req, res) => {
-  let session = {
-    nick: 'guest'
-  };
-  db.generate_session(session);
+	let session = {
+	  nick: 'guest'
+	};
+	db.generate_session(session);
 
-  res.send(JSON.stringify(session));
+	res.send(JSON.stringify(session));
 });
 
 app.listen(8080);
